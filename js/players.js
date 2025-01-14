45d1c09c659e4d777709faec7307f5a108dd7ee8
PieroRadice <58561490+PieroRadice@users.noreply.github.com>
2025-01-14 14:13:50 +0100

    1

---
+++ js/players.js
@@ -0,0 +1,16 @@
+export const players = {
+  "Jannik Sinner": "https://www.atptour.com/-/media/alias/player-headshot/s0ag",
+  "Alexander Zverev":
+    "https://www.atptour.com/-/media/alias/player-headshot/z355",
+  "Carlos Alcaraz":
+    "https://www.atptour.com/-/media/alias/player-headshot/a0e2",
+  "Taylor Fritz": "https://www.atptour.com/-/media/alias/player-headshot/fb98",
+  "Daniil Medvedev":
+    "https://www.atptour.com/-/media/alias/player-headshot/mm58",
+  "Novak Djokovic":
+    "https://www.atptour.com/-/media/alias/player-headshot/d643",
+  "Andrey Rublev": "https://www.atptour.com/-/media/alias/player-headshot/re44",
+  "Jack Draper": "https://www.atptour.com/-/media/alias/player-headshot/D0CO",
+  "Lorenzo Musetti":
+    "https://www.atptour.com/-/media/alias/player-headshot/m0ej",
+};
