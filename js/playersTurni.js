c4cf4aaa5e4e869fc2af6763f92ba72b07e59075
PieroRadice <58561490+PieroRadice@users.noreply.github.com>
2025-01-21 16:45:55 +0100

    QUARTI

---
+++ js/playersTurni.js
@@ -0,0 +1,56 @@
+export const playersTurni = {
+  "Jannik Sinner": {
+    turno: 5,
+    giocato: 0,
+    vinto: 1,
+    img: "https://www.atptour.com/-/media/alias/player-headshot/s0ag",
+  },
+  "Alexander Zverev": {
+    turno: 6,
+    giocato: 0,
+    vinto: 1,
+    img: "https://www.atptour.com/-/media/alias/player-headshot/z355",
+  },
+  "Carlos Alcaraz": {
+    turno: 5,
+    giocato: 1,
+    vinto: 0,
+    img: "https://www.atptour.com/-/media/alias/player-headshot/a0e2",
+  },
+  "Taylor Fritz": {
+    turno: 3,
+    giocato: 1,
+    vinto: 0,
+    img: "https://www.atptour.com/-/media/alias/player-headshot/fb98",
+  },
+  "Daniil Medvedev": {
+    turno: 2,
+    giocato: 1,
+    vinto: 0,
+    img: "https://www.atptour.com/-/media/alias/player-headshot/mm58",
+  },
+  "Novak Djokovic": {
+    turno: 6,
+    giocato: 0,
+    vinto: 1,
+    img: "https://www.atptour.com/-/media/alias/player-headshot/d643",
+  },
+  "Andrey Rublev": {
+    turno: 1,
+    giocato: 1,
+    vinto: 0,
+    img: "https://www.atptour.com/-/media/alias/player-headshot/re44",
+  },
+  "Jack Draper": {
+    turno: 4,
+    giocato: 1,
+    vinto: 0,
+    img: "https://www.atptour.com/-/media/alias/player-headshot/D0CO",
+  },
+  "Lorenzo Musetti": {
+    turno: 3,
+    giocato: 1,
+    vinto: 0,
+    img: "https://www.atptour.com/-/media/alias/player-headshot/m0ej",
+  },
+};
--- js/turni.js
+++ js/turni.js
@@ -1,128 +1,8 @@
-const playersTurni = {
-  "Jannik Sinner": {
-    turno: 5,
-    giocato: 0,
-    vinto: 1,
-    img: "https://www.atptour.com/-/media/alias/player-headshot/s0ag",
-  },
-  "Alexander Zverev": {
-    turno: 5,
-    giocato: 0,
-    vinto: 1,
-    img: "https://www.atptour.com/-/media/alias/player-headshot/z355",
-  },
-  "Carlos Alcaraz": {
-    turno: 5,
-    giocato: 0,
-    vinto: 1,
-    img: "https://www.atptour.com/-/media/alias/player-headshot/a0e2",
-  },
-  "Taylor Fritz": {
-    turno: 3,
-    giocato: 1,
-    vinto: 0,
-    img: "https://www.atptour.com/-/media/alias/player-headshot/fb98",
-  },
-  "Daniil Medvedev": {
-    turno: 2,
-    giocato: 1,
-    vinto: 0,
-    img: "https://www.atptour.com/-/media/alias/player-headshot/mm58",
-  },
-  "Novak Djokovic": {
-    turno: 5,
-    giocato: 0,
-    vinto: 1,
-    img: "https://www.atptour.com/-/media/alias/player-headshot/d643",
-  },
-  "Andrey Rublev": {
-    turno: 1,
-    giocato: 1,
-    vinto: 0,
-    img: "https://www.atptour.com/-/media/alias/player-headshot/re44",
-  },
-  "Jack Draper": {
-    turno: 4,
-    giocato: 1,
-    vinto: 0,
-    img: "https://www.atptour.com/-/media/alias/player-headshot/D0CO",
-  },
-  "Lorenzo Musetti": {
-    turno: 3,
-    giocato: 1,
-    vinto: 0,
-    img: "https://www.atptour.com/-/media/alias/player-headshot/m0ej",
-  },
-};
+import { predictions } from "./predictions.js";
+import { playersTurni } from "./playersTurni.js";
+const punteggi = [];
 
-const predictions = [
-  {
-    name: "Pier",
-    img: "https://www.tennisworlditalia.com/imgb/40869/la-rivista-gq-descrive-federer-come-il-migliore-di-sempre-e-partono-le-critiche.webp",
-    winner: "Jannik Sinner",
-    semi1: "Carlos Alcaraz",
-    semi2: "Daniil Medvedev",
-    semi3: "Novak Djokovic",
-  },
-  {
-    name: "Pam",
-    img: "https://static2.amica.it/wp-content/uploads/2023/07/8456559_large.jpg?v=1278015_1688727790",
-    winner: "Carlos Alcaraz",
-    semi1: "Jannik Sinner",
-    semi2: "Alexander Zverev",
-    semi3: "Daniil Medvedev",
-  },
-  {
-    name: "Piero",
-    img: "immagini/agassi.png",
-    winner: "Taylor Fritz",
-    semi1: "Jannik Sinner",
-    semi2: "Jack Draper",
-    semi3: "Lorenzo Musetti",
-  },
-  {
-    name: "Lombo",
-    img: "https://c7.alamy.com/compit/gbwtye/tennis-wimbledon-championships-mens-singles-final-bjorn-borg-v-john-mcenroe-bjorn-borg-in-azione-gbwtye.jpg",
-    winner: "Carlos Alcaraz",
-    semi1: "Jannik Sinner",
-    semi2: "Alexander Zverev",
-    semi3: "Novak Djokovic",
-  },
-  {
-    name: "Mix",
-    img: "https://compass-media.vogue.it/photos/6481dae3e03ab1e612092926/3:4/w_1920,c_limit/3241967",
-    winner: "Jannik Sinner",
-    semi1: "Taylor Fritz",
-    semi2: "Carlos Alcaraz",
-    semi3: "Andrey Rublev",
-  },
-  {
-    name: "Rob",
-    img: "immagini/wavrinka.png",
-    winner: "Alexander Zverev",
-    semi1: "Carlos Alcaraz",
-    semi2: "Jannik Sinner",
-    semi3: "Andrey Rublev",
-  },
-  {
-    name: "Cuzzo",
-    img: "immagini/macroe.png",
-    winner: "Jannik Sinner",
-    semi1: "Taylor Fritz",
-    semi2: "Carlos Alcaraz",
-    semi3: "Alexander Zverev",
-  },
-  {
-    name: "Gigi",
-    img: "https://static.wixstatic.com/media/aff08a_abda19b7314f4c42a58085b83414859f~mv2.png/v1/crop/x_40,y_0,w_830,h_1270/fill/w_634,h_970,al_c,q_90,usm_0.66_1.00_0.01,enc_avif,quality_auto/Pete%20Sampras%2C%20%40Tennis.png",
-    winner: "Jannik Sinner",
-    semi1: "Carlos Alcaraz",
-    semi2: "Daniil Medvedev",
-    semi3: "Andrey Rublev",
-  },
-];
-
-export function risultatoTurno(playerNome, turnoChar) {
+function risultatoTurno(playerNome, turnoChar) {
   const turno = parseInt(turnoChar);
   if (playersTurni[playerNome].turno > turno) return "ok";
   if (playersTurni[playerNome].turno < turno) return "uscito";
@@ -145,7 +25,7 @@ export function risultatoTurno(playerNome, turnoChar) {
     return "esce";
 }
 // Assumendo che tu abbia una funzione per aggiungere i giocatori alla DOM
-export function aggiornaStileGiocatore(playerElement, risultatoTurno) {
+function aggiornaStileGiocatore(playerElement, risultatoTurno) {
   if (!playerElement) return;
 
   // Rimuovi tutte le classi specifiche
@@ -168,7 +48,26 @@ export function aggiornaStileGiocatore(playerElement, risultatoTurno) {
       break;
   }
 }
-export function createCell(turno, title, playerNome, isWinner = false) {
+function calcolaPunteggioGiocatore(turno, playerNome) {
+  switch (risultatoTurno(playerNome, turno)) {
+    case "uscito":
+      return false;
+      break;
+    case "esce":
+      return false;
+      break;
+    case "inGara":
+      return true;
+      break;
+    case "ok":
+      return true;
+      break;
+    default:
+      return false;
+      break;
+  }
+}
+function createCell(turno, title, playerNome, isWinner = false) {
   const cell = document.createElement("div");
   cell.className = isWinner ? "winner" : "cell";
   aggiornaStileGiocatore(cell, risultatoTurno(playerNome, turno));
@@ -187,6 +86,8 @@ export function creaColonne(turno) {
 
   predictions.forEach((prediction) => {
     //creazione header con dati dello scommettitore
+    punteggi[prediction.name] = 0;
+
     const column = document.createElement("div");
     column.className = "column";
 
@@ -197,30 +98,41 @@ export function creaColonne(turno) {
     ${prediction.name}
   `;
     column.appendChild(headerCell);
+    const punteggioCell = document.createElement("div");
+    punteggioCell.className = "punteggio";
+    headerCell.appendChild(punteggioCell);
     //creazione della pila dei tennisti
     const content = document.createElement("div");
     content.className = "content";
-
-    if (prediction.winner)
+    if (prediction.winner) {
       content.appendChild(
         createCell(turno, "VINCITORE", prediction.winner, true)
       );
-
-    if (prediction.semi1)
+      punteggi[prediction.name] +=
+        calcolaPunteggioGiocatore(turno, prediction.winner) * 125;
+    }
+    if (prediction.semi1) {
       content.appendChild(
         createCell(turno, "ALTRO SEMIFINALISTA", prediction.semi1)
       );
-
-    if (prediction.semi2)
+      punteggi[prediction.name] +=
+        calcolaPunteggioGiocatore(turno, prediction.semi1) * 25;
+    }
+    if (prediction.semi2) {
       content.appendChild(
         createCell(turno, "ALTRO SEMIFINALISTA", prediction.semi2)
       );
-
-    if (prediction.semi3)
+      punteggi[prediction.name] +=
+        calcolaPunteggioGiocatore(turno, prediction.semi2) * 25;
+    }
+    if (prediction.semi3) {
       content.appendChild(
         createCell(turno, "ALTRO SEMIFINALISTA", prediction.semi3)
       );
-
+      punteggi[prediction.name] +=
+        calcolaPunteggioGiocatore(turno, prediction.semi3) * 25;
+    }
+    punteggioCell.innerHTML = `${punteggi[prediction.name]}`;
     column.appendChild(content);
     columnContainer.appendChild(column);
   });
